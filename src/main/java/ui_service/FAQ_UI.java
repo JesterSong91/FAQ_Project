b5d6cbe3aaa3fd3fb35f3c12c822ffc5e39b6d80
Sergei Bashkintcev <jestersong@mail.ru>
2023-10-06 11:57:03 +0400

    Implemented show selected table row in JTextArea controls

--- src/main/java/ui_service/FAQ_UI.java
+++ src/main/java/ui_service/FAQ_UI.java
@@ -4,6 +4,8 @@ import entity.QuestionAnswer;
 import ui.QuestionAnswerOperation;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableRowSorter;
 import java.awt.event.ActionEvent;
@@ -34,6 +36,8 @@ public class FAQ_UI {
     private String filterText;
 
     private TableRowSorter myRowSorter;
+
+    private DefaultTableModel dtm;
 
     public FAQ_UI() {
         qao = new QuestionAnswerOperation();
@@ -84,13 +88,12 @@ public class FAQ_UI {
     }
 
     public void initTable() {
-        DefaultTableModel dtm = new DefaultTableModel();
+        dtm = new DefaultTableModel();
         dtm.setColumnIdentifiers(new String[] {"Question", "Answer", "Answer Code"});
 
         List<QuestionAnswer> data = qao.findAllData();
 
-        for (QuestionAnswer curr_elem : data
-        ) {
+        for (QuestionAnswer curr_elem : data) {
             dtm.addRow(new String[] {curr_elem.getQuestionText(), curr_elem.getAnswerText(), curr_elem.getAnswerExampleCode()});
         }
 
@@ -98,6 +101,71 @@ public class FAQ_UI {
 
         AnswersTable.setModel(dtm);
         AnswersTable.setRowSorter(myRowSorter);
+
+        ListSelectionModel rowSelectionModel = AnswersTable.getSelectionModel();
+        rowSelectionModel.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                StringBuilder output = new StringBuilder();
+
+                int chosenRowIndex = 0;
+
+                ListSelectionModel lsm = (ListSelectionModel)e.getSource();
+
+                int firstIndex = e.getFirstIndex();
+                int lastIndex = e.getLastIndex();
+                boolean isAdjusting = e.getValueIsAdjusting();
+                output.append("Event for indexes "
+                        + firstIndex + " - " + lastIndex
+                        + "; isAdjusting is " + isAdjusting
+                        + "; selected indexes:");
+
+                if (lsm.isSelectionEmpty()) {
+                    output.append(" <none>");
+                } else {
+                    // Find out which indexes are selected.
+                    int minIndex = lsm.getMinSelectionIndex();
+                    int maxIndex = lsm.getMaxSelectionIndex();
+                    for (int i = minIndex; i <= maxIndex; i++) {
+                        if (lsm.isSelectedIndex(i)) {
+                            output.append(" " + i);
+                            chosenRowIndex = i;
+                        }
+                    }
+                }
+
+                System.out.println("dtm: " + dtm);
+
+                try {
+                    questionText = dtm.getValueAt(chosenRowIndex, 0).toString();
+                }
+                catch (NullPointerException ex){
+                    questionText = new String();
+                }
+
+                try {
+                    answerText = dtm.getValueAt(chosenRowIndex, 1).toString();
+                }
+                catch (NullPointerException ex){
+                    answerText = new String();
+                }
+
+                try {
+                    answerCodeText = dtm.getValueAt(chosenRowIndex, 2).toString();
+                }
+                catch (NullPointerException ex) {
+                    answerCodeText = new String();
+                }
+
+                System.out.println("questionText: " + questionText);
+                System.out.println("answerText: " + answerText);
+                System.out.println("answerCodeText: " + answerCodeText);
+
+                QuestionTextArea.setText(questionText);
+                AnswerTextArea.setText(answerText);
+                AnswerCodeTextArea.setText(answerCodeText);
+            }
+        });
     }
 
     public RowFilter rowFilterFactory(String filterText) {
